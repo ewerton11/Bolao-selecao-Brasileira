75f7c1b7a11234240944ca36393ddbf01d97f3d9
ewerton11 <ewertonreisc0@gmail.com>
2022-11-06 16:27:33 -0300

    alteraçao html

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <html>
+    <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -19,6 +19,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-    </html>
+    </Html>
   )
 }
